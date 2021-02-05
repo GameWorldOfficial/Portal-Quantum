c7a2f0ea4a48bb671b855aba85986fb41dc0c25d
GameWorld <46016751+GameWorldOfficial@users.noreply.github.com>
2021-02-05 22:33:32 +1000

    Adding gamefiles

---
+++ Portals-master/Assets/Scripts/ColiderScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColiderScript : MonoBehaviour
+{
+
+    public GameObject PortalTrigger1;
+    public GameObject PortalTrigger2;
+    public GameObject Wallrooffloor1;
+    public GameObject Wallrooffloor2;
+
+    void OnTriggerEnter(Collider other)
+    {
+        PortalTrigger1.GetComponent<BoxCollider>().enabled = true;
+        PortalTrigger2.GetComponent<BoxCollider>().enabled = true;
+        Wallrooffloor1.GetComponent<MeshCollider>().enabled = false;
+        Wallrooffloor2.GetComponent<MeshCollider>().enabled = false;
+    }
+
+    void Start()
+    {
+        PortalTrigger1.GetComponent<BoxCollider>().enabled = true;
+        PortalTrigger2.GetComponent<BoxCollider>().enabled = true;
+        Wallrooffloor1.GetComponent<MeshCollider>().enabled = true;
+        Wallrooffloor2.GetComponent<MeshCollider>().enabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        PortalTrigger1.GetComponent<BoxCollider>().enabled = true;
+        PortalTrigger2.GetComponent<BoxCollider>().enabled = true;
+        Wallrooffloor1.GetComponent<MeshCollider>().enabled = true;
+        Wallrooffloor2.GetComponent<MeshCollider>().enabled = true;
+    }
+
+}
---
+++ Portals-master/Assets/Scripts/PortalGunRayCastTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalGunRayCastTest : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+
+            hit.collider.gameObject.GetComponent<MeshRenderer>();
+
+            print("Material Name: " + hit.collider.gameObject.GetComponent<MeshRenderer>().material.name);
+        }
+    }
+}
